be45869b4dafbb820cbf9089b3563574dfbfd8f1
Charlie-Bryant <38361562+Charlie-Bryant@users.noreply.github.com>
2019-03-26 14:42:40 +0000

    Add files via upload
    added func

---
+++ FlaskWeatherStation/getweatherapi.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+import requests 
+import simplejson as json
+
+
+def getweatherapi(Location):
+	Weather_api = 'https://api.openweathermap.org/data/2.5/weather?APPID=89da9ee78fce51911e8ca20b9906d704&q='
+	# Address+Location Returns location relevant data
+	User_Weather_api = Weather_api + Location
+	#Resposne Request Returned as .Json file
+	Weather = requests.get(User_Weather_api).json()
+
+	weather_type_json = Weather['weather'][0]['main']          
+	tempreture_json = float(Weather['main']['temp'])
+	
+	weatherapidic = {
+	"weathertype" : weather_type_json ,
+	"tempreture" : tempreture_json
+	}
+
+	return weatherapidic
--- FlaskWeatherStation/main.py
+++ FlaskWeatherStation/main.py
@@ -3,19 +3,10 @@ import requests
 import simplejson as json
 import xml.etree.ElementTree as etree
 from urllib.request import urlopen
+from getweatherapi import *
 
 
 app = Flask(__name__)
-
-# Connects to weatherapi.com and returns json of Weather Data
-def getweatherapi(Location):
-	
-	Weather_api ='https://api.openweathermap.org/data/2.5/weather?APPID=89da9ee78fce51911e8ca20b9906d704&q='
-	# Address+Location Returns location relevant data
-	User_Weather_api = Weather_api + Location
-	#Resposne Request Returned as .Json file 
-	Weather = requests.get(User_Weather_api).json()
-	return Weather
 
 # Connects to BBC weather and returns xml of Weather Data
 def getbbcweather():
@@ -23,23 +14,13 @@ def getbbcweather():
 	tree = etree.parse(urlopen(weather_bbc))
 	return tree
 
-#Method:Random Activity Return: Activity
-
-
-
-#Method: Sorts Dictionary Variables Return: Variable 
-
-
 #@app.home//
 @app.route("/", methods=['GET', 'POST'])
 def home():
 
 	Weather = getweatherapi('Cambridge')
-	weather_type = Weather['weather'][0]['main']
-	tempreture = float(Weather['main']['temp'])
 
-
-	return render_template('home.html', title = 'home', weather_type = weather_type, tempreture=tempreture)
+	return render_template('home.html', weather = Weather)
 
 if __name__ == '__main__':
 	app.run(debug='true')
